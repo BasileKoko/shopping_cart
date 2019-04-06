8c5bf5f01a28239323559687892a052c46e7203f
Basile Koko <basile_koko@hotmail.com>
2019-04-06 23:44:17 +0100

    add user registration

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('items/', views.ItemView.as_view(), name='item_view'),
     path('items/<int:pk>/', views.ItemDetailView.as_view()),
     path('rest-auth/', include('rest_auth.urls')),
-
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
--- shopping/settings.py
+++ shopping/settings.py
@@ -37,12 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shopping',
-    'api',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+    'shopping',
+    'api',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
